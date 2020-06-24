95b0af05e39f515eb9ba514a661ddd4a5420eaee
Kai Chen <kaichen@outlook.com>
2020-06-24 16:06:01 -0700

    fix: Minor display fix

--- pkg/terminal/return.go
+++ pkg/terminal/return.go
@@ -52,7 +52,8 @@ func (viewer *ReturnViewer) drawPerformance() {
 	}
 
 	setString(viewer.table, viewer.performance.Portfolio.Name, 1, 0, tcell.ColorWhite, tview.AlignLeft)
-	setPercentChange(viewer.table, viewer.performance.Result.Return.Max, 1, 10)
+	setPercentChange(viewer.table, viewer.performance.Result.Return.Max, 1, 9)
 
 	setString(viewer.table, viewer.performance.Benchmark.Portfolio.Name, 2, 0, tcell.ColorWhite, tview.AlignLeft)
+	setPercentChange(viewer.table, viewer.performance.Benchmark.Return.Max, 2, 9)
 }
