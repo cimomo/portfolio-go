bcb832c5a46b036ce8fb3a171751a0256f193251
Kai Chen <kaichen@outlook.com>
2020-11-04 12:10:24 -0800

    chore: Minor tweaks to the help message

--- pkg/terminal/help.go
+++ pkg/terminal/help.go
@@ -15,13 +15,13 @@ type HelpViewer struct {
 func NewHelpViewer() *HelpViewer {
 	return &HelpViewer{
 		help: map[string]string{
-			"h":        "This help message",
-			"0/m":      "Home page",
-			"1...9":    "Switch to portfolio",
-			"r":        "Reload profile",
-			"q/Ctrl+c": "Exit",
+			"<h>":            "This help message",
+			"<0>/<m>":        "Switch to home page",
+			"<1>...<9>":      "Switch to portfolio",
+			"<r>":            "Reload profile",
+			"<q>/<Ctrl>+<c>": "Exit",
 		},
-		table: tview.NewTable().SetBorders(true),
+		table: tview.NewTable().SetBorders(false),
 	}
 }
 
@@ -29,9 +29,11 @@ func NewHelpViewer() *HelpViewer {
 func (viewer *HelpViewer) Draw() {
 	viewer.table.Clear()
 
+	viewer.table.SetTitle("Help").SetBorder(true).SetBorderPadding(1, 1, 2, 2).SetBackgroundColor(tcell.ColorBlue)
+
 	r := 0
 	for k, v := range viewer.help {
-		setString(viewer.table, k, r, 0, tcell.ColorWhite, tview.AlignRight)
+		setString(viewer.table, k, r, 0, tcell.ColorWhite, tview.AlignLeft)
 		setString(viewer.table, v, r, 1, tcell.ColorWhite, tview.AlignLeft)
 		r++
 	}
